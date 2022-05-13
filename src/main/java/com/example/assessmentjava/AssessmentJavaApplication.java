b5bf8c0251cd55c8fc39db32ddfd78c79d0a2f3e
AndreaScaffidiAbbate <andrea.scaffidi92@gmail.com>
2022-05-13 09:28:45 +0200

    Branch and Project

---
+++ src/main/java/com/example/assessmentjava/AssessmentJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.assessmentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/assessmentjava/AssessmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assessmentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
