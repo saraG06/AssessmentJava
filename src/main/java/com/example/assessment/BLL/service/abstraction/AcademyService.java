20018f1471fe1e0c97e1d2725fb97bec06f1ed41
Matteo Fabio <macbookmatteo@MacBook-Air-di-Matteo.local>
2022-05-13 17:48:45 +0200

    fine assessment

--- src/main/java/com/example/assessment/BLL/service/abstraction/AcademyService.java
+++ src/main/java/com/example/assessment/BLL/service/abstraction/AcademyService.java
@@ -2,7 +2,6 @@ package com.example.assessment.BLL.service.abstraction;
 
 import com.example.assessment.BLL.dto.request.AcademyRequestDTO;
 import com.example.assessment.BLL.dto.response.AcademyResponseDTO;
-import com.example.assessment.DAL.entity.Academy;
 
 public interface AcademyService {
 
