a8fc8107be6542596d85b80bfe5521d7e74f7e3c
giorgio_carpoca <g.carpoca@gmail.com>
2022-05-13 09:47:31 +0200

    first commit

---
+++ src/main/java/it/unikey/assestmentjava/AssestmentJavaApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.assestmentjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssestmentJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssestmentJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unikey/assestmentjava/AssestmentJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.assestmentjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssestmentJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
