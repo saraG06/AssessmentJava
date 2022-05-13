6c38e17ebd2718d4f9bebe4730cbfbea70533939
NeroKaios <matteo_samoila@yahoo.it>
2022-05-13 12:09:08 +0200

    Crud Controller Done

--- src/main/java/it/unikey/PL/controller/DocenteController.java
+++ src/main/java/it/unikey/PL/controller/DocenteController.java
@@ -1,4 +1,39 @@
 package it.unikey.PL.controller;
 
+import it.unikey.BLL.dto.request.DocenteRequestDTO;
+import it.unikey.BLL.dto.response.DocenteResponseDTO;
+import it.unikey.BLL.exception.IdNotFoundException;
+import it.unikey.BLL.service.abstraction.DocenteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/docente")
+@CrossOrigin("http://localhost:8080")
 public class DocenteController {
+    private final DocenteService docenteService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody DocenteRequestDTO docenteRequestDTO){
+        docenteService.saveDocente(docenteRequestDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<DocenteResponseDTO> getById(@PathVariable Long id) throws IdNotFoundException {
+        return new ResponseEntity<>(docenteService.findById(id),HttpStatus.OK);
+    }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id) throws IdNotFoundException{
+        docenteService.deleteDocente(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<DocenteResponseDTO>> findAll(){
+        return new ResponseEntity<>(docenteService.findAllDocente(),HttpStatus.OK);
+    }
 }
--- src/main/java/it/unikey/PL/controller/ModuloController.java
+++ src/main/java/it/unikey/PL/controller/ModuloController.java
@@ -1,4 +1,40 @@
 package it.unikey.PL.controller;
 
+import it.unikey.BLL.dto.request.ModuloRequestDTO;
+import it.unikey.BLL.dto.response.ModuloResponseDTO;
+import it.unikey.BLL.exception.IdNotFoundException;
+import it.unikey.BLL.service.abstraction.ModuloService;
+import it.unikey.DAL.entity.Modulo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/modulo")
+@CrossOrigin("http://localhost:8080")
 public class ModuloController {
+    private final ModuloService moduloService;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody ModuloRequestDTO moduloRequestDTO){
+        moduloService.saveModulo(moduloRequestDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<ModuloResponseDTO> getById(@PathVariable Long id) throws IdNotFoundException {
+        return new ResponseEntity<>(moduloService.findById(id),HttpStatus.OK);
+    }
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Long> delete(@PathVariable Long id) throws IdNotFoundException{
+        moduloService.deleteModulo(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<ModuloResponseDTO>> findAll(){
+        return new ResponseEntity<>(moduloService.findAllModulo(),HttpStatus.OK);
+    }
 }
