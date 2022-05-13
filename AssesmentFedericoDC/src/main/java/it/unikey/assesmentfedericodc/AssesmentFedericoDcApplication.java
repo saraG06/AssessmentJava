875ff744f115c272da91f4dea7555b86ebb3a472
federicodecaro <federicodcr96@outlook.it>
2022-05-13 09:26:57 +0200

    primoCommit

---
+++ AssesmentFedericoDC/src/main/java/it/unikey/assesmentfedericodc/AssesmentFedericoDcApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.assesmentfedericodc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssesmentFedericoDcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssesmentFedericoDcApplication.class, args);
+    }
+
+}
---
+++ AssesmentFedericoDC/src/test/java/it/unikey/assesmentfedericodc/AssesmentFedericoDcApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.assesmentfedericodc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssesmentFedericoDcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
