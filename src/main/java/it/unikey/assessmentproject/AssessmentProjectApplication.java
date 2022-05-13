a4973cfff6534bc2d73de8d6cbd9131ca086ebdc
catellopalatucci <catellopalatucci@outlook.it>
2022-05-13 09:43:01 +0200

    PrimoCommit

---
+++ src/main/java/it/unikey/assessmentproject/AssessmentProjectApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.assessmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unikey/assessmentproject/AssessmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.assessmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
