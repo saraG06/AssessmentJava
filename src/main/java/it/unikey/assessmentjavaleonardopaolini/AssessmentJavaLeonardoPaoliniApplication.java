d56724429037d3317779d171291abb91b4fbd27d
DESKTOP-GO6P2KJ\EmilianoUnikey <luca.papaleorm@gmail.com>
2022-05-13 09:28:51 +0200

    primo commit

---
+++ src/main/java/it/unikey/assessmentjavaleonardopaolini/AssessmentJavaLeonardoPaoliniApplication.java
@@ -0,0 +1,13 @@
+package it.unikey.assessmentjavaleonardopaolini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentJavaLeonardoPaoliniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssessmentJavaLeonardoPaoliniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/unikey/assessmentjavaleonardopaolini/AssessmentJavaLeonardoPaoliniApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unikey.assessmentjavaleonardopaolini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentJavaLeonardoPaoliniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
